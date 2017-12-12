9478e49c9f1ab0f1b2e02eb83b716a9e0a73602a
Nicolas Herbaut <nherbaut@labri.fr>
2017-12-12 16:54:45 +0100

    default site

--- mcr/libsettings.py
+++ mcr/libsettings.py
@@ -9,6 +9,7 @@ settings_pwd = settings["password"]
 settings_ssh_key_file_public = settings["ssh_key_file_public"]
 settings_ssh_key_file_private = settings["ssh_key_file_private"]
 settings_g5k_alias = settings["grid5k-ProxyCommand-domain-alias"]
-settings_ = settings["environment"]
+settings_environment = settings["environment"]
+settings_default_site = settings["default-site"]
 with open(settings_ssh_key_file_public) as f:
     settings_ssh_key = f.read()
